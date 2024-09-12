dd29c6ab35ce76dbaa7e78894fc9129240074f48
Veeresh Hiremath <85979428+codingveer@users.noreply.github.com>
2024-09-12 15:18:11 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+import { BunchOfStuff, OtherStuffAlsoComplicated } from "./components/mocks";
+import { VerySlowComponent } from "./components/very-slow-component";
+import "./styles.scss";
+
+const MovingBlock = ({ position }: { position: number }) => (
+  <div className="movable-block" style={{ top: position }}>
+    {position}
+  </div>
+);
+
+// just hard-coded approximation to demonstrate the re-renders problem
+// not to be used in real code
+const getPosition = (val: number) => 150 - val / 2;
+
+export default function App() {
+  const [position, setPosition] = useState(150);
+
+  const onScroll = (e: any) => {
+    // calculate position based on the scrolled value
+    const calculated = getPosition(e.target.scrollTop);
+    // save it to state
+    setPosition(calculated);
+  };
+
+  return (
+    <div className="scrollable-block" onScroll={onScroll}>
+      {/* pass position value to the new movable component */}
+      <MovingBlock position={position} />
+      <VerySlowComponent />
+      <BunchOfStuff />
+      <OtherStuffAlsoComplicated />
+    </div>
+  );
+}
